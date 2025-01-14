7e320895a898823ddf53ff43b2f15b594ef3d508
MateusNazare <mateus.nazare5@gmail.com>
2025-01-13 23:52:36 -0300

    feat: add register expense

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+
+    }
+}

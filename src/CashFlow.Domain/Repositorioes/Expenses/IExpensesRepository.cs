e2c6ffdf77a2d54b48ee3aeb874e5936af8f3482
MateusNazare <mateus.nazare5@gmail.com>
2025-03-28 18:13:09 -0300

    wip

--- src/CashFlow.Domain/Repositorioes/Expenses/IExpensesRepository.cs
+++ src/CashFlow.Domain/Repositorioes/Expenses/IExpensesRepository.cs
@@ -4,4 +4,8 @@ namespace CashFlow.Domain.Repositorioes.Expenses;
 public interface IExpensesRepository
 {
     Task Add(Expense expense);
+
+    Task<List<Expense>> GetAll();
+
+    Task<Expense?> GetById(long id);
 }
